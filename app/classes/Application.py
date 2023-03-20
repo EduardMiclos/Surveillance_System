809df415e6bc22b3b5c0616b00207b78f2f60e5f
Ubuntu <miclosedi@SurveillanceSystem.iiuzif24tkxunmmhsk2qvccuzd.fx.internal.cloudapp.net>
2023-03-20 15:36:09 +0000

    Finished the Application class
    This class starts the Flask application after starting
    the Web Server Gateway Interface using gunicorn. It
    also loads all the resources to the RESTful API.

--- app/classes/Application.py
+++ app/classes/Application.py
@@ -1,4 +1,5 @@
-import subprocess
+from flask import Flask
+from flask_restful import Api, Resource
 import WSGI
 
 class Application:
@@ -6,38 +7,43 @@ class Application:
 
     Application
 
-    Application class used for initializing and managing a Flask application,
-    using a certain number of RESTful resources.
+    Application class used for initializing and managing a Flask application.
 
     Args:
-        app_module (str): The name of the Python module that contains the
-                          Flask application.
-        application_instance (str):The variable name of the Flask 
-                          application instance within that module.
         port (str): The output port.
-
     Methods:
-        __init__(app_module: str, application_instance: str, port: str): 
-                          Instantiates the class variables.
-        run(): Performs a try-catch operation and tries 
-                          to run the command using subprocess.
+        __init__(port: str): Instantiates the class variables.
+        run(debug: bool): Starts the Flask application.
 
     """
-    def __init__(app_module: str = "app", application_instance: str = "app", port: str = "8080"):
-        self.app_module = app_module
-        self.application_instance = application_instance
+    def __init__(port: str = "8080"):
         self.port = port
     
-    def run():
-        try:
-            subprocess.run(['gunicorn', 
-                            f'{self.app_module}:{self.application_instance}', 
-                            '--bind',
-                            f'0.0.0.0:{self.port}'], check = True)
-        except subprocess.CalledProcessError as err:
-            print(f'ERROR: Occured when trying to initialize a gateway interface.
-                    Error code: {err.returncode}\n 
-                    Error output: {err.output}')
+    def run(self, resources: object, debug: bool = False):
+        """
+        Creating and running the web server gateway interface.
+        """
+        self.wsgi = WSGI(port = self.port)
+        self.wsgi.run()
+
+        """
+        Creating the Flask application + restful API.
+        """
+        self.app = Flask(__name__)
+        self.api = Api(app)
+
+        """
+        Iterating through all the resources (classes)
+        and adding them to the REST API object.
+        """
+        for resource in resources:
+            self.api.add_resource(resource, resource.getUrl())
+
+        """
+        Running the app
+        """
+        self.app.run(debug)
+
             
 
         
