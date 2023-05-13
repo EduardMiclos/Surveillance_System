2a2c4a626d83d619625ff76a4d99f6c3f942f19e
Eduard Miclos <micloseduard@yahoo.com>
2023-05-13 14:31:14 +0000

    Added the Response class
    This class creates a standard response that
    is slightly customizable by each request. This ensures
    a basic format for each request.

---
+++ app/blueprints/api/controllers/Response.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+class Response:
+    def __init__(self):
+        self.json = {}
+       
+        
+    def get_code(self):
+        return self.json['Code']
+
+    def set_code(self, code):
+        self.json['Code'] = code
+
+        
+    def get_status(self):
+        return self.json['Status']
+    
+    def set_status(self, status):
+        self.json['Status'] = status
+
+         
+    def get_message(self):
+        return self.json['Message']
+    
+    def set_message(self, message):
+        self.json['Message'] = message
+    
+        
+    def add_data(self, key, value):
+        if 'Data' not in self.json:
+            self.json['Data'] = {}
+            
+        self.json['Data'][key] = value
+        
+    def get_data(self):
+        return self.json['Data']
+    
+    def get_response(self):
+        self.json['Date'] = datetime.now()
+        return jsonify(self.json)
+    
+    def set_success(self):
+        self.set_code(200)
+        self.set_message('Success')
+        
