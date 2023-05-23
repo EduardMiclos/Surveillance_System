fe1489ba8f2e20ba6355a3fdcd38abb6b87fbe48
Eduard Miclos <micloseduard@yahoo.com>
2023-05-23 18:31:05 +0000

    Added password hashing

--- app/application/database/models/User.py
+++ app/application/database/models/User.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin
 from sqlalchemy import CheckConstraint
 from sqlalchemy.ext.hybrid import hybrid_property
 from validate_email_address import validate_email
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from ..database import *
 
@@ -12,6 +13,12 @@ class User(db.Model, UserMixin):
     secondname = db.Column(db.String(50), nullable=False)
     password_hash = db.Column(db.String(80), nullable=False)
     is_admin = db.Column(db.Boolean(), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     @hybrid_property
     def is_email_valid(self):
