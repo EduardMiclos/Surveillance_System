c9f0704355c1e511ded123e03ae94280e47ed5c4
Eduard Miclos <micloseduard@yahoo.com>
2023-05-29 20:33:44 +0000

    Changed __init__ function in EventStreamer
    Added parameters.

--- app/application/controllers/EventStreamer/EventStreamer.py
+++ app/application/controllers/EventStreamer/EventStreamer.py
@@ -1,14 +1,14 @@
 from flask_sse import sse
 
 class EventStreamer:
-    def __init__(self) -> None:
-        self.data = {}
-        self.type = None  
+    def __init__(self, data, event_type) -> None:
+        self.data = data
+        self.event_type = event_type
     
     def stream(self):
         sse.publish(
             data = self.data, 
-            type = self.type)
+            type = self.event_type)
         
     
     
