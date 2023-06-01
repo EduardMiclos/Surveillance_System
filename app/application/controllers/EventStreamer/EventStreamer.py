3ffea91ca2dcfbda5a932cd5f9cea82d59e17cd1
Eduard Miclos <micloseduard@yahoo.com>
2023-06-01 19:23:17 +0000

    Updated EventStreamer events

--- app/application/controllers/EventStreamer/EventStreamer.py
+++ app/application/controllers/EventStreamer/EventStreamer.py
@@ -1,14 +1,18 @@
 from flask_sse import sse
+import json
 
 class EventStreamer:
-    def __init__(self, data, event_type) -> None:
-        self.data = data
+    def __init__(self, event_type) -> None:
+        self.data = {}
         self.event_type = event_type
     
     def stream(self):
         sse.publish(
             {
                 "message": self.event_type,
-                "data": self.data
+                "data": json.dumps(self.data)
             },
             type = self.event_type)
+        
+    def add_data(self, key, value):
+        self.data[key] = value
--- app/application/controllers/EventStreamer/EventStreamerFactory.py
+++ app/application/controllers/EventStreamer/EventStreamerFactory.py
@@ -3,11 +3,12 @@ from .EventType import EventType
 
 class EventStreamerFactory:
     event_mapping = {
-            EventType.HW_DATA_TRANSMISSION_ALTER: {"data": "", "type": 'HW_DATA_TRANSMISSION_ALTER'},
-            EventType.HW_UPDATE: {"data": "<here to be added paths to preprocess files that were updated>", "type": 'HW_UPDATE'},
-            EventType.MANAGE_USER_REFRESH: {"data": "", "type": 'USER_REFRESH'},
-            EventType.MANAGE_FOOTAGES_REFRESH: {"data": "", "type": 'FOOTAGES_REFRESH'},
-            EventType.MANAGE_CAMERA_REFRESH: {"data": "", "type": 'CAMERA_REFRESH'}
+            EventType.HW_DATA_TRANSMISSION_ALTER: {"type": 'HW_DATA_TRANSMISSION_ALTER'},
+            EventType.RASP_UPDATE: {"type": 'RASP_UPDATE'},
+            EventType.RASP_REMOVE: {"type": 'RASP_REMOVE'},
+            EventType.MANAGE_USER_REFRESH: {"type": 'USER_REFRESH'},
+            EventType.MANAGE_FOOTAGES_REFRESH: {"type": 'FOOTAGES_REFRESH'},
+            EventType.MANAGE_CAMERA_REFRESH: {"type": 'CAMERA_REFRESH'}
         }
     
     def __init__(self):
@@ -16,8 +17,7 @@ class EventStreamerFactory:
     @staticmethod
     def create(event_type):
         if event_type in EventStreamerFactory.event_mapping:
-            data = EventStreamerFactory.event_mapping[event_type]["data"]
             event_type = EventStreamerFactory.event_mapping[event_type]["type"]
-            return EventStreamer(data, event_type)
+            return EventStreamer(event_type)
         else:
             raise ValueError(f'ERROR: Invalid event type! The valid event types are {EventStreamerFactory.event_mapping}')
